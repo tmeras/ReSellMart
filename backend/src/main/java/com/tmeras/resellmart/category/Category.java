c0825b8d1c192602e7aa2c1575d0b020a994a998
tmeras <tmeras@yahoo.gr>
2025-01-23 11:22:04 +0200

    Implement category entity

---
+++ backend/src/main/java/com/tmeras/resellmart/category/Category.java
@@ -0,0 +1,24 @@
+package com.tmeras.resellmart.category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    // TODO: Add relationship to product entity
+
+}
