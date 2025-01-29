7430dd1819f20dcb6083dd04d3c6d8f18b6d38e8
tmeras <tmeras@yahoo.gr>
2025-01-29 18:19:52 +0200

    Implement product repository

---
+++ backend/src/main/java/com/tmeras/resellmart/product/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.tmeras.resellmart.product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query("""
+            SELECT p FROM Product p
+            WHERE p.available = true AND p.seller.id <> :sellerId
+    """)
+    Page<Product> findAllBySellerIdNot(Pageable pageable, Integer sellerId);
+
+    @Query("""
+            SELECT p FROM Product p
+            WHERE p.available = true AND p.seller.id = :sellerId
+    """)
+    Page<Product> findAllBySellerId(Pageable pageable, Integer sellerId);
+
+    @Query("""
+            SELECT p FROM Product p
+            WHERE p.available = true AND p.seller.id <> :sellerId
+            AND p.category.id = :categoryId
+    """)
+    Page<Product> findAllByCategoryId(Pageable pageable, Integer categoryId, Integer sellerId);
+}
