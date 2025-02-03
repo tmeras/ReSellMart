f07f10adaaad494a9c0f78c44340f0d2dbd42f6b
tmeras <tmeras@yahoo.gr>
2025-02-03 15:53:14 +0200

    Implement service tests for saving a new category

--- backend/src/test/java/com/tmeras/resellmart/category/CategoryServiceTests.java
+++ backend/src/test/java/com/tmeras/resellmart/category/CategoryServiceTests.java
@@ -1,6 +1,8 @@
 package com.tmeras.resellmart.category;
 
 import com.tmeras.resellmart.TestDataUtils;
+import com.tmeras.resellmart.exception.APIException;
+import com.tmeras.resellmart.exception.ResourceAlreadyExistsException;
 import com.tmeras.resellmart.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,10 +41,59 @@ public class CategoryServiceTests {
         // Initialise test objects
         categoryA = TestDataUtils.createCategoryA();
         categoryB = TestDataUtils.createCategoryB();
+        categoryB.setParentCategory(categoryA);
+
         categoryRequestA = TestDataUtils.createCategoryRequestA();
         categoryRequestB = TestDataUtils.createCategoryRequestB();
+        categoryRequestB.setParentId(categoryRequestA.getId());
+
         categoryResponseA = TestDataUtils.createCategoryResponseA();
         categoryResponseB = TestDataUtils.createCategoryResponseB();
+        categoryResponseB.setParentId(categoryResponseA.getId());
+    }
+
+    @Test
+    public void shouldSaveCategoryWhenValidCategory() {
+        when(categoryRepository.findByName(categoryRequestA.getName())).thenReturn(Optional.empty());
+        when(categoryMapper.toCategory(categoryRequestA)).thenReturn(categoryA);
+        when(categoryRepository.save(categoryA)).thenReturn(categoryA);
+        when(categoryMapper.toCategoryResponse(categoryA)).thenReturn(categoryResponseA);
+
+        CategoryResponse categoryResponse = categoryService.save(categoryRequestA);
+
+        assertThat(categoryResponse).isEqualTo(categoryResponseA);
+    }
+
+    @Test
+    public void shouldNotSaveCategoryWhenDuplicateCategoryName() {
+        when(categoryRepository.findByName(categoryRequestA.getName())).thenReturn(Optional.of(categoryA));
+
+        assertThatThrownBy(() -> categoryService.save(categoryRequestA))
+                .isInstanceOf(ResourceAlreadyExistsException.class);
+    }
+
+    @Test
+    public void shouldNotSaveCategoryWhenInvalidParentId() {
+        when(categoryRepository.findByName(categoryRequestB.getName())).thenReturn(Optional.empty());
+        when(categoryRepository.findById(categoryRequestA.getId())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> categoryService.save(categoryRequestB))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    public void shouldNotSaveCategoryWhenInvalidParent() {
+        Category categoryC = new Category(3, "Test category A", null);
+        categoryA.setParentCategory(categoryC);
+
+        // Evaluate that an expression is thrown if the parent category
+        // of the category being saved also has a parent
+        when(categoryRepository.findByName(categoryRequestB.getName())).thenReturn(Optional.empty());
+        when(categoryRepository.findById(categoryRequestA.getId())).thenReturn(Optional.of(categoryA));
+
+        assertThatThrownBy(() -> categoryService.save(categoryRequestB))
+                .isInstanceOf(APIException.class)
+                .withFailMessage("Parent category should not have a parent");
     }
 
     @Test
@@ -62,6 +113,4 @@ public class CategoryServiceTests {
         assertThatThrownBy(() -> categoryService.findById(99))
                 .isInstanceOf(ResourceNotFoundException.class);
     }
-
-
 }
