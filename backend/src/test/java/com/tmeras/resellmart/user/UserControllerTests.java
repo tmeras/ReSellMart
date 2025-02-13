228a5de21f0b3ffd508f33fdfc20ded2e099855c
tmeras <tmeras@yahoo.gr>
2025-02-13 12:08:40 +0200

    Implement controller tests for saving, updating, and deleting a cart item and for finding all of a user's cart items

--- backend/src/test/java/com/tmeras/resellmart/user/UserControllerTests.java
+++ backend/src/test/java/com/tmeras/resellmart/user/UserControllerTests.java
@@ -2,9 +2,14 @@ package com.tmeras.resellmart.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tmeras.resellmart.TestDataUtils;
+import com.tmeras.resellmart.cart.CartItemRequest;
+import com.tmeras.resellmart.cart.CartItemResponse;
+import com.tmeras.resellmart.category.Category;
 import com.tmeras.resellmart.common.AppConstants;
 import com.tmeras.resellmart.common.PageResponse;
 import com.tmeras.resellmart.configuration.TestConfig;
+import com.tmeras.resellmart.product.Product;
+import com.tmeras.resellmart.product.ProductResponse;
 import com.tmeras.resellmart.role.Role;
 import com.tmeras.resellmart.token.JwtFilter;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +21,7 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -26,6 +32,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,7 +41,7 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -57,6 +64,7 @@ public class UserControllerTests {
     @MockitoBean
     private UserService userService;
 
+    private ProductResponse productResponseA;
     private User userA;
     private UserRequest userRequestA;
     private UserResponse userResponseA;
@@ -67,11 +75,15 @@ public class UserControllerTests {
         // Initialise test objects
         Role adminRole = new Role(1, "ADMIN");
         Role userRole = new Role(2, "USER");
-
         userA = TestDataUtils.createUserA(Set.of(adminRole));
         userRequestA = TestDataUtils.createUserRequestA();
         userResponseA = TestDataUtils.createUserResponseA(Set.of(adminRole));
         userResponseB = TestDataUtils.createUserResponseB(Set.of(userRole));
+
+        productResponseA = TestDataUtils.createProductResponseA(
+                TestDataUtils.createCategoryResponseA(),
+                userResponseA
+        );
     }
 
     @Test
@@ -161,6 +173,98 @@ public class UserControllerTests {
         assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(userResponseA));
     }
 
+    @Test
+    public void shouldSaveCartItemWhenValidRequest() throws Exception {
+        CartItemRequest cartItemRequest = new CartItemRequest(productResponseA.getId(), 1, userA.getId());
+        CartItemResponse cartItemResponse = new CartItemResponse(1, productResponseA, 1, LocalDateTime.now());
 
+        when(userService.saveCartItem(any(CartItemRequest.class), eq(userA.getId()), any(Authentication.class)))
+                .thenReturn(cartItemResponse);
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/users/" + userA.getId() + "/cart/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(cartItemRequest))
+        ).andExpect(status().isCreated()).andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(cartItemResponse));
+    }
+
+    @Test
+    public void shouldNotSaveCartItemWhenInvalidRequest() throws Exception {
+        CartItemRequest cartItemRequest = new CartItemRequest(null, 1, userA.getId());
+        Map<String, String> expectedErrors = new HashMap<>();
+        expectedErrors.put("productId", "Product ID must not be empty");
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/users/" + userA.getId() + "/cart/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(cartItemRequest))
+        ).andExpect(status().isBadRequest()).andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(expectedErrors));
+    }
+
+    @Test
+    public void shouldFindALLCartItemsByUserId() throws Exception {
+        List<CartItemResponse> cartItemResponses = List.of(
+                new CartItemResponse(1, productResponseA, 1, LocalDateTime.now())
+        );
+
+        when(userService.findAllCartItemsByUserId(eq(userA.getId()), any(Authentication.class)))
+                .thenReturn(cartItemResponses);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/users/" + userA.getId() + "/cart/products"))
+                .andExpect(status().isOk())
+                .andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(cartItemResponses));
+    }
+
+    @Test
+    public void shouldUpdateCartItemQuantityWhenValidRequest() throws  Exception {
+        CartItemRequest cartItemRequest = new CartItemRequest(productResponseA.getId(), 5, userA.getId());
+        CartItemResponse cartItemResponse = new CartItemResponse(1, productResponseA, 5, LocalDateTime.now());
+
+        when(userService.updateCartItemQuantity(
+                any(CartItemRequest.class), eq(userA.getId()), eq(productResponseA.getId()), any(Authentication.class))
+        ).thenReturn(cartItemResponse);
+
+        MvcResult mvcResult = mockMvc.perform(
+                patch("/api/users/" + userA.getId() + "/cart/products/" + productResponseA.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cartItemRequest))
+        ).andExpect(status().isOk()).andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(cartItemResponse));
+    }
+
+    @Test
+    public void shouldNotUpdateCartItemQuantityWhenInvalidRequest() throws Exception {
+        CartItemRequest cartItemRequest = new CartItemRequest(null, 5, userA.getId());
+        Map<String, String> expectedErrors = new HashMap<>();
+        expectedErrors.put("productId", "Product ID must not be empty");
+
+        MvcResult mvcResult = mockMvc.perform(
+                patch("/api/users/" + userA.getId() + "/cart/products/" + productResponseA.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cartItemRequest))
+        ).andExpect(status().isBadRequest()).andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(expectedErrors));
+    }
+
+    @Test
+    public void shouldDeleteCartItem() throws  Exception {
+        mockMvc.perform(
+                delete("/api/users/" + userA.getId() + "/cart/products/" + productResponseA.getId())
+        ).andExpect(status().isNoContent());
+
+        verify(userService, times(1))
+                .deleteCartItem(eq(userA.getId()), eq(productResponseA.getId()), any(Authentication.class));
+    }
 
 }
