7b1020f07a9c19804fa86fb6b63109beab1b65ed
tmeras <tmeras@yahoo.gr>
2025-02-19 17:54:02 +0200

    Implement controller tests for finding all address,  all addresses by user ID, and all non-deleted addresses by user ID

--- backend/src/test/java/com/tmeras/resellmart/address/AddressControllerTests.java
+++ backend/src/test/java/com/tmeras/resellmart/address/AddressControllerTests.java
@@ -2,6 +2,8 @@ package com.tmeras.resellmart.address;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tmeras.resellmart.TestDataUtils;
+import com.tmeras.resellmart.common.AppConstants;
+import com.tmeras.resellmart.common.PageResponse;
 import com.tmeras.resellmart.configuration.TestConfig;
 import com.tmeras.resellmart.token.JwtFilter;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,11 +21,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -85,6 +91,59 @@ public class AddressControllerTests {
 
         assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(expectedErrors));
     }
+
+    @Test
+    public void shouldFindAllAddresses() throws Exception {
+        PageResponse<AddressResponse> pageResponse = new PageResponse<>(
+                List.of(addressResponseA, addressResponseB),
+                AppConstants.PAGE_NUMBER_INT, AppConstants.PAGE_SIZE_INT,
+                2, 1,
+                true, true
+        );
+
+        when(addressService.findAll(
+                AppConstants.PAGE_NUMBER_INT, AppConstants.PAGE_SIZE_INT,
+                AppConstants.SORT_ADDRESSES_BY, AppConstants.SORT_DIR
+        )).thenReturn(pageResponse);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/addresses"))
+                .andExpect(status().isOk())
+                .andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(pageResponse));
+    }
+
+    @Test
+    public void shouldFindAllAddressesByUserId() throws Exception {
+        List<AddressResponse> addressResponses = List.of(addressResponseA);
+
+        when(addressService.findAllByUserId(eq(addressResponseA.getUserId()), any(Authentication.class)))
+                .thenReturn(addressResponses);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/users/" + addressResponseA.getUserId() + "/addresses"))
+                .andExpect(status().isOk())
+                .andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(addressResponses));
+    }
+
+    @Test
+    public void shouldFindAllNonDeletedAddressesByUserId() throws Exception {
+        List<AddressResponse> addressResponses = List.of(addressResponseA);
+
+        when(addressService.findAllNonDeletedByUserId(eq(addressResponseA.getUserId()), any(Authentication.class)))
+                .thenReturn(addressResponses);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/users/" + addressResponseA.getUserId() + "/addresses/non-deleted"))
+                .andExpect(status().isOk())
+                .andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        assertThat(jsonResponse).isEqualTo(objectMapper.writeValueAsString(addressResponses));
+    }
+
 
 
 }
